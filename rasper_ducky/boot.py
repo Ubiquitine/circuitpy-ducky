1547b3352f213e4a3d0d9370cdd037ad337ee513
Berlioz Ubiquitus <github@ubiquitine.anonaddy.me>
2025-11-19 15:28:49 +0800

    Change the NO_STORGE_PIN for better comatibility with Waveshare RP2040-one

--- rasper_ducky/boot.py
+++ rasper_ducky/boot.py
@@ -2,17 +2,10 @@ import board
 import digitalio
 import storage
 
-NO_STORAGE_PIN = digitalio.DigitalInOut(board.GP15)
+NO_STORAGE_PIN = digitalio.DigitalInOut(board.GP1)
 NO_STORAGE_PIN.switch_to_input(pull=digitalio.Pull.UP)
 no_storage_status = NO_STORAGE_PIN.value
 
-
-if board.board_id == "raspberry_pi_pico":
-    # On Pi Pico, default to USB visible
-    no_storage = not no_storage_status
-else:
-    no_storage = False
-
-
+no_storage = not no_storage_status
 if no_storage:
     storage.disable_usb_drive()
