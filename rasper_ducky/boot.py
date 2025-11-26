6a4395f7df09efb1784b89e63377f69b5c5b5542
Ubiquitine <evgeniy.gurevich@gmail.com>
2025-11-26 22:03:07 +0800

    Added ARMED switch

--- rasper_ducky/boot.py
+++ rasper_ducky/boot.py
@@ -2,10 +2,13 @@ import board
 import digitalio
 import storage
 
+# Enable payload execution
+ARMED=False
+
 NO_STORAGE_PIN = digitalio.DigitalInOut(board.GP29)
 NO_STORAGE_PIN.switch_to_input(pull=digitalio.Pull.UP)
 no_storage_status = NO_STORAGE_PIN.value
 
 no_storage = no_storage_status
-if no_storage:
+if no_storage and ARMED:
     storage.disable_usb_drive()
